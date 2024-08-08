951a72df66031dd59ceb9b8138b2a4136b7948af
mancinibrzl <mariomancinineto2017@gmail.com>
2024-08-08 18:32:07 -0300

    Update ItemCollectableCoin.cs

--- Astral Leap Saga - EBAC/Assets/Scripts/Items/ItemCollectableCoin.cs
+++ Astral Leap Saga - EBAC/Assets/Scripts/Items/ItemCollectableCoin.cs
@@ -6,9 +6,41 @@ public class ItemCollectableCoin : ItemCollectableBase
 {
     public Collider Collider;
 
+    public bool collect = false;
+
+    public float lerp = 5f;
+    public float minDistance = 1f;
+
+    private void Start()
+    {
+        //CoinsAnimationManager.Instance.RegisterCoin(this);
+    }
+
+    protected override void Collect()
+    {
+        OnCollect();
+    }
+
+    private void Update()
+    {
+        if (collect)
+        {
+            transform.position = Vector3.Lerp(transform.position, PlayerController.Instance.transform.position, lerp * Time.deltaTime);
+        }
+
+        if (Vector3.Distance(transform.position, PlayerController.Instance.transform.position)<minDistance)
+        {
+            //HideItens();
+            Destroy(gameObject);
+        }
+    }
+
     protected override void OnCollect()
     {
         base.OnCollect();
+        GetComponent<Collider>().enabled = false;
+        collect = true;
+        //PlayerController.Instance.Bounce();
         ItemManager.Instance.AddCoins();
         GetComponent<Collider>().enabled = false;
     }
