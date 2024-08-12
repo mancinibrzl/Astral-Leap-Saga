c807c6f1c8c6b4f1e489fce7122e04fd247a9439
mancinibrzl <mariomancinineto2017@gmail.com>
2024-08-11 21:54:41 -0300

    Update AnimatorManager.cs

--- Astral Leap Saga - EBAC/Assets/Scripts/Animation/AnimatorManager.cs
+++ Astral Leap Saga - EBAC/Assets/Scripts/Animation/AnimatorManager.cs
@@ -14,13 +14,14 @@ public class AnimatorManager : MonoBehaviour
         DEATH
     }
 
-    public void Play(AnimationType type)
+    public void Play(AnimationType type, float currentSpeedFactor = 1f)
     {
         foreach (var animation in animatorSetups)
         {
             if (animation.type == type)
             {
                 animator.SetTrigger(animation.trigger);
+                animator.speed = animation.speed * currentSpeedFactor;
                 break;
             }
         }
@@ -47,5 +48,6 @@ public class AnimatorManager : MonoBehaviour
     {
         public AnimatorManager.AnimationType type;
         public string trigger;
+        public float speed = 1f;
     }
 }
