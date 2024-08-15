cb0ff91b65fd8efe7fc6d27ae959c7b6a15e3b38
mancinibrzl <mariomancinineto2017@gmail.com>
2024-08-15 13:12:34 -0300

    Update SOLevelPieceBasedSetup.cs

--- Astral Leap Saga - EBAC/Assets/Scripts/Utils/ScriptableObjects/SOLevelPieceBasedSetup.cs
+++ Astral Leap Saga - EBAC/Assets/Scripts/Utils/ScriptableObjects/SOLevelPieceBasedSetup.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 [CreateAssetMenu]
 public class SOLevelPieceBasedSetup : ScriptableObject
 {
+    public ArtManager.ArtType artType;
+
     [Header("Pieces")]
     public List<LevelPieceBase> levelPiecesStart;
     public List<LevelPieceBase> levelPieces;
