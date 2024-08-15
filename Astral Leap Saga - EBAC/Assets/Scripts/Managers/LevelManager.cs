6fcafbe87b4356eeb13b96ec6c4a68375c9d701e
mancinibrzl <mariomancinineto2017@gmail.com>
2024-08-15 13:12:31 -0300

    Update LevelManager.cs

--- Astral Leap Saga - EBAC/Assets/Scripts/Managers/LevelManager.cs
+++ Astral Leap Saga - EBAC/Assets/Scripts/Managers/LevelManager.cs
@@ -97,6 +97,15 @@ public class LevelManager : MonoBehaviour
 
             spawnedPiece.transform.position = lastPiece.endPiece.position;
         }
+        else
+        {
+            spawnedPiece.transform.localPosition = Vector3.zero;
+        }
+
+        foreach (var p in spawnedPiece.GetComponentsInChildren<ArtPiece>())
+        {
+            p.ChangePiece(ArtManager.Instance.GetSetupByType(_currSetup.artType).gameObject);
+        }
         
         _spawnedPieces.Add(spawnedPiece);
 
